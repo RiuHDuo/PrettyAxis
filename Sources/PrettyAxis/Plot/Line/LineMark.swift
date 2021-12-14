d5c91260b2e63f8a524239cfae286996cfc35001
RiuHDuo <fearmyone@gmail.com>
2021-12-14 17:41:47 +0800

    delete drawing a wrong line mark

--- Sources/PrettyAxis/Plot/Line/LineMark.swift
+++ Sources/PrettyAxis/Plot/Line/LineMark.swift
@@ -22,11 +22,10 @@ struct LineMark:Shape{
             return Path()
         }
         let unit = rect.height / (range.max - range.min)
-        var points = (0 ..< self.points.count).map({ i -> CGPoint in
+        let points = (0 ..< self.points.count).map({ i -> CGPoint in
             let axisData = self.points[i]
             return CGPoint(x:CGFloat(i) * spacing + spacing / 2, y: rect.size.height - (axisData.yValue - CGFloat(range.min)) * unit * animatableData)
         })
-        points.insert(CGPoint(x: 0, y: points.first?.y ?? 0), at: 0)
         
         return Path{ p in
             points.forEach({p.addPath(mark, transform: CGAffineTransform(translationX: $0.x, y: $0.y))})
